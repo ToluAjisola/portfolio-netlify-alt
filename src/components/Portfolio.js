1d1f7b3190783bcce5a6ec6a4a50fc8cd3b755c7
ToluAjisola <toluwalopeajisola@gmail.com>
2023-08-05 23:22:11 -0400

    Portfolio fix

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "./assets/css/portfolio.css";
 import thumbnail1 from "../components/assets/images/pic01.jpg";
 
@@ -6,20 +6,24 @@ function Portfolio() {
   return (
     <>
       <section id="portfolio">
-        <div className="content">
-          <h2>Projects</h2>
-          <div className="portfolio-wrapper">
-            <div className="screenshot-wrapper">
-              <img src={thumbnail1} alt="My portfolio profile" />
-            </div>
-            <div className="detail-wrapper">
-              <p>Project description</p>
+        <div className="portfolio-wrapper">
+          <div className="screenshot-wrapper">
+            <img src={thumbnail1} alt="My portfolio profile" />
+          </div>
+          <div className="content">
+            {/* <h2>Projects</h2> */}
+            <div className="inner">
+              <div className="detail-wrapper">
+                <h2>Project name</h2>
+                <p>Project description</p>
+              </div>
             </div>
           </div>
         </div>
+        
       </section>
     </>
   );
 }
 
-export default Portfolio
+export default Portfolio;
